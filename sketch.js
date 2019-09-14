cdb9ff47f7104379d6ae3b993cac8e7b7769eaf2
Tomasz Ociepka <tomipl200@o2.pl>
2019-09-14 10:27:40 +0200

    Camera can be moved vertically

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ const c = 30;
 const G = 3;
 
 var bh;
+var camDir;
+var camPos;
 
 const size = 400;
 
@@ -19,19 +21,22 @@ function setup() {
     new Photon(x , y, PI);
   }*/
 
-  let cx = width;
-  let cy = height/2;
+  camPos = createVector(width, height/2 - 20);
 
-  let a0 = PI;
-  let am = 14/12 * PI;
+  camDir = p5.Vector.sub(bh.pos, camPos);
+
+  let a0 = camDir.heading();
+  let am = a0 +1/6 * PI;
 
   let da = (am - a0)/width/sqrt(2)*2*0.99;
 
   for(let a = a0 ; a < am; a += da){
-    new Photon(cx,cy,a);
+    new Photon(camPos.x, camPos.y,a);
   }
   print(photons.length);
   print(width);
+
+  camDir.normalize();
   
   ellipseMode(RADIUS);
 
@@ -51,7 +56,7 @@ function setup() {
   }
 
   render();
-  //raytrace(200,0);
+  //raytrace(0,200);
 }
 
 function render(){
@@ -78,21 +83,37 @@ function raytrace(pixel_x,pixel_y){
   let photon = photons[ Math.round(pixelPos.mag())];
   //return color(pixelPos.mag());
   let angle = -pixelPos.heading();
-  //angle += HALF_PI;
+  angle -= HALF_PI;
   //return color( sin(angle)*255);
-  let co = cos(angle);
-  let si = sin(angle);
+  let cosine = cos(angle);
+  let sine= sin(angle);
+
+  var xt1 = (camDir.x*camDir.y*(1-cosine)-camDir.z*sine);
+  var xt2 = (cosine + camDir.x*camDir.x*(1-cosine));
+
+  var yt1 = (cosine + camDir.y*camDir.y*(1-cosine));
+  var yt2 = (camDir.x*camDir.y*(1-cosine)+camDir.z*sine);
+
+  var zt1 = (camDir.z*camDir.y*(1-cosine)+camDir.x*sine);
+  var zt2 = (camDir.z*camDir.x*(1-cosine)-camDir.y*sine);
 
   let hit = "sky";
-  //stroke(0,0,255);
   var dis;
 
   for(let pos of photon.path){
-    let x = pos.x;
-    let y = si * (pos.y-width/2) + width/2;
-    let z = -co * (pos.y-width/2);
+
+    let v = pos.copy().sub(camPos);
+    let d = v.dot(camDir);
+    let linePoint = camPos.copy().add(camDir.copy().mult(d));
+    let off = pos.copy().sub(linePoint);
+
+    let x = xt1 * off.y + xt2 * off.x;
+    let y = yt1 * off.y + yt2 * off.x;
+    let z = zt1 * off.y + zt2 * off.x;
+
     let pos3d = createVector(x,y,z);
-    //point(x,y);
+    pos3d.add(linePoint);
+    //point(pos3d.x,pos3d.y);
     
     disSq = p5.Vector.sub(bh.pos, pos3d).magSq();
 
@@ -101,10 +122,9 @@ function raytrace(pixel_x,pixel_y){
       hit = "hole";
       break;
     }
-
   
-    if(y < bh.pos.y + 5 && y > bh.pos.y - 5)
-    if( (x-bh.pos.x)*(x-bh.pos.x) + (y-bh.pos.y)*(y-bh.pos.y) < 9*bh.rs*bh.rs) {
+    if(pos3d.y < bh.pos.y + 3 && pos3d.y > bh.pos.y - 3)
+    if( (pos3d.x-bh.pos.x)*(pos3d.x-bh.pos.x) + (pos3d.y-bh.pos.y)*(pos3d.y-bh.pos.y) < 9*bh.rs*bh.rs) {
       hit = "disk";
       break;
     }
@@ -118,7 +138,7 @@ function raytrace(pixel_x,pixel_y){
   }
 
   if(hit == "hole") return color(0);
-  return color(0);
+  return color(10);
 }
 
 
